f2c9faa388cd1d51e68fa75971a2ee3a26776350
ElizabethM.Coss <maciascoss.me@gmail.com>
2022-09-18 13:35:06 -0500

    Replace data into my basic elements with js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Lunes",
+    "Martes",
+    "Miércoles",
+    "Jueves",
+    "Viernes",
+    "Sábado",
+    "Domingo",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = (document.querySelector("#temperature").innerHTML =
     Math.round(response.data.main.temp));
   let cityElement = (document.querySelector("#city").innerHTML =
@@ -12,8 +34,12 @@ function displayTemperature(response) {
   let windSpeed = (document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   ));
+  let dateElement = (document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
+  ));
 }
 let apiKey = "b02cfae02de3053b0adae5c03c2e38dc";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
